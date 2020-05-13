ed3d6963a95af53256b5d8c06d37d3d4ac0e3dae
lwijnsma <l.l.wijnsma@st.hanze.nl>
2020-05-13 19:10:59 +0200

    klasse Artikel

---
+++ src/main/java/Artikel.java
@@ -0,0 +1,28 @@
+public class Artikel {
+
+    public String naam;
+    public Double prijs;
+
+    public Artikel(String naam, Double prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public Double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(Double prijs) {
+        this.prijs = prijs;
+    }
+
+
+}
