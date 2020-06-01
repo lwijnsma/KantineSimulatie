617421f73b7ed75b46ea9676a0a1bc764d5008f8
lwijnsm <l.l.wijnsma@st.hanze.nl>
2020-06-01 14:44:37 +0200

    Week 4 opdracht 1

--- src/main/java/Pinpas.java
+++ src/main/java/Pinpas.java
@@ -1,20 +1,26 @@
-//public class Pinpas extends Betaalwijze {
-//
-//    private double kredietlimiet;
-//
-//    /**
-//     * Methode om kredietlimiet te zetten
-//     *
-//     * @param kredietlimiet
-//     */
-//    public void setKredietLimiet(double kredietlimiet) {
-//        // method body omitted
-//    }
-//
-//    /**
-//     * Methode om betaling af te handelen
-//     */
-//    public boolean betaal(double tebetalen) {
-//        // method body omitted
-//    }
-//}
+public class Pinpas extends Betaalwijze {
+
+    private double kredietlimiet;
+
+    /**
+     * Methode om kredietlimiet te zetten
+     *
+     * @param kredietlimiet
+     */
+    public void setKredietLimiet(double kredietlimiet) {
+        this.kredietlimiet=kredietlimiet;
+    }
+
+    /**
+     * Methode om betaling af te handelen
+     */
+    public boolean betaal(double tebetalen) {
+      if(saldo > tebetalen){
+       if(tebetalen <= kredietlimiet){
+          setSaldo(saldo-tebetalen);
+          return true;
+        } else return false;
+
+      } else return false;
+    }
+}
