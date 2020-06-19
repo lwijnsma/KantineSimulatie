fb055011068fa6703db177ab0280afac118052d9
lwijnsm <l.l.wijnsma@st.hanze.nl>
2020-06-19 11:53:01 +0200

    removed factuur regel annotations

--- src/main/java/FactuurRegel.java
+++ src/main/java/FactuurRegel.java
@@ -1,21 +1,12 @@
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name = "factuurregel")
 public class FactuurRegel implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
     private Long id;
 
-    @ManyToOne(targetEntity = Factuur.class, cascade = CascadeType.ALL)
-    @Column(name = "Factuur_idFactuur")
     private Factuur factuur;
 
-    @Embedded
-    @Column(name = "Artikel_idArtikel")
     public Artikel artikel;
 
     public FactuurRegel(){}
